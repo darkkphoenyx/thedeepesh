023687671c427c931b4d54d01a390b09a1f09712
Deepesh Sunuwar <deepgeneral33@gmail.com>
2025-11-21 15:57:46 +0545

    fix: hola

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,3 @@
 export default {
   extends: ["@commitlint/config-conventional"],
-  rules: {
-    "type-empty": [0], // 0 = turn off
-    "subject-empty": [0], // 0 = turn off
-    "header-max-length": [2, "always", 100], // optional: customize max length
-  },
 };
