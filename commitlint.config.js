70f901ffe2de8f2908ca118fb0c3aacec3cb2d00
Deepesh Sunuwar <deepgeneral33@gmail.com>
2025-11-21 15:56:32 +0545

    asdjkf

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,8 @@
-export default { extends: ["@commitlint/config-conventional"] };
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-empty": [0], // 0 = turn off
+    "subject-empty": [0], // 0 = turn off
+    "header-max-length": [2, "always", 100], // optional: customize max length
+  },
+};
