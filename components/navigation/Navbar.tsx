c34e5b5eef03b1fbf8bc7865b4b9a988d1b99be9
Deepesh Sunuwar <deepgeneral33@gmail.com>
2026-01-29 16:11:50 +0545

    refactor: code refactored in Navbar

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { useNavigation } from "@/hooks/useNavigation";
-import { NavigationInterface } from "@/interfaces/navigation";
+import {
+  DesktopNavLinksInterface,
+  MobileMenuInterface,
+  MobileNavLinksInterface,
+  NavigationInterface,
+} from "@/interfaces/navigation";
 import Heading from "../../shared/Heading";
 import { NavRoutes } from "../data/NavRoutes";
 
@@ -27,7 +32,7 @@ const Navbar = ({ sectionRefs }: NavigationInterface) => {
           : "bg-background"
       }`}
     >
-      <div className="mx-[10px]">
+      <div className="mx-2.5">
         <div
           className={`flex items-center justify-between w-full mx-auto py-4 md:px-4 pl-2 transition-all duration-300 ease-in-out`}
           style={{
@@ -38,92 +43,27 @@ const Navbar = ({ sectionRefs }: NavigationInterface) => {
           <Logo navigation={handleRefNavigation} />
 
           {/* Desktop Nav Links */}
-          <div className="hidden md:block">
-            <ul className="flex gap-10 text-lg">
-              {NavRoutes.map((link, index) => (
-                <li
-                  data-aos="fade-down"
-                  data-aos-delay={`${index * 50}`}
-                  data-aos-once="true"
-                  key={link.id}
-                >
-                  <a
-                    href={`#${link.link}`}
-                    className={`transition hover:text-secondary cursor-none ${
-                      activeSection === link.link
-                        ? "font-medium text-secondary"
-                        : ""
-                    }`}
-                    onClick={(e) => {
-                      e.preventDefault();
-                      handleRefNavigation(link.link);
-                    }}
-                  >
-                    {link.nav}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
+          <DesktopNavLinks
+            activeSection={activeSection}
+            handleRefNavigation={handleRefNavigation}
+          />
 
           {/* Mobile Menu Toggle */}
-          <div className="md:hidden" id="mobile-menu">
-            {/* <button
-              onClick={toggleMobileMenu}
-              aria-expanded={isMobileMenuOpen}
-              aria-controls="mobile-menu"
-              aria-label="Toggle mobile navigation menu"
-            >
-              {isMobileMenuOpen ? <X size={36} /> : <Menu size={36} />}
-            </button> */}
-            <button
-              className="flex flex-col gap-2"
-              onClick={toggleMobileMenu}
-              aria-expanded={isMobileMenuOpen}
-              aria-controls="mobile-menu"
-              aria-label="Toggle mobile navigation menu"
-            >
-              {lines.map((cls, i) => (
-                <div
-                  key={i}
-                  className={`h-[2px] bg-primary w-7 transition-all rounded-full duration-300 ${cls}`}
-                />
-              ))}
-            </button>
-          </div>
+          <MobileMenu
+            isMobileMenuOpen={isMobileMenuOpen}
+            lines={lines}
+            toggleMobileMenu={toggleMobileMenu}
+          />
         </div>
       </div>
 
       {/* Mobile Menu */}
       {isMobileMenuOpen && (
-        <div className="md:hidden px-4 pb-4 w-full overflow-hidden">
-          <ul className="flex flex-col gap-4">
-            {NavRoutes.map((link, index) => (
-              <li
-                data-aos="fade-left"
-                data-aos-delay={`${index * 50}`}
-                data-aos-duration="400"
-                key={link.id}
-              >
-                <a
-                  href={`#${link.link}`}
-                  className={`block w-full py-2 ${
-                    activeSection === link.link
-                      ? "font-medium text-secondary"
-                      : ""
-                  }`}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setIsMobileMenuOpen(false);
-                    handleRefNavigation(link.link);
-                  }}
-                >
-                  {link.nav}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <MobileNavLinks
+          activeSection={activeSection}
+          handleRefNavigation={handleRefNavigation}
+          setIsMobileMenuOpen={setIsMobileMenuOpen}
+        />
       )}
     </nav>
   );
@@ -150,6 +90,99 @@ const Logo = ({ navigation }: { navigation: any }) => {
           once
         />
       </a>
+    </div>
+  );
+};
+
+const DesktopNavLinks = ({
+  activeSection,
+  handleRefNavigation,
+}: DesktopNavLinksInterface) => {
+  return (
+    <div className="hidden md:block">
+      <ul className="flex gap-10 text-lg">
+        {NavRoutes.map((link, index) => (
+          <li
+            data-aos="fade-down"
+            data-aos-delay={`${index * 50}`}
+            data-aos-once="true"
+            key={link.id}
+          >
+            <a
+              href={`#${link.link}`}
+              className={`transition hover:text-secondary cursor-none ${
+                activeSection === link.link ? "font-medium text-secondary" : ""
+              }`}
+              onClick={(e) => {
+                e.preventDefault();
+                handleRefNavigation(link.link);
+              }}
+            >
+              {link.nav}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const MobileMenu = ({
+  toggleMobileMenu,
+  isMobileMenuOpen,
+  lines,
+}: MobileMenuInterface) => {
+  return (
+    <div className="md:hidden" id="mobile-menu">
+      <button
+        className="flex flex-col gap-2"
+        onClick={toggleMobileMenu}
+        aria-expanded={isMobileMenuOpen}
+        aria-controls="mobile-menu"
+        aria-label="Toggle mobile navigation menu"
+      >
+        {lines.map((cls, i) => (
+          <div
+            key={i}
+            className={`h-0.5 bg-primary w-7 transition-all rounded-full duration-300 ${cls}`}
+          />
+        ))}
+      </button>
+    </div>
+  );
+};
+
+const MobileNavLinks = ({
+  activeSection,
+  setIsMobileMenuOpen,
+  handleRefNavigation,
+}: MobileNavLinksInterface) => {
+  return (
+    <div className="md:hidden px-4 pb-4 w-full overflow-hidden">
+      <ul className="flex flex-col gap-4">
+        {NavRoutes.map((link, index) => (
+          <li
+            data-aos="fade-left"
+            data-aos-delay={`${index * 50}`}
+            data-aos-duration="400"
+            key={link.id}
+          >
+            <a
+              href={`#${link.link}`}
+              className={`block w-full py-2 ${
+                activeSection === link.link ? "font-medium text-secondary" : ""
+              }`}
+              onClick={(e) => {
+                e.preventDefault();
+                setIsMobileMenuOpen(false);
+                handleRefNavigation(link.link);
+              }}
+            >
+              {link.nav}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- interfaces/navigation.ts
+++ interfaces/navigation.ts
@@ -1,3 +1,22 @@
+import { Dispatch, SetStateAction } from "react";
+
 export interface NavigationInterface {
   sectionRefs: React.RefObject<Record<string, HTMLDivElement | null>>;
+}
+
+export interface DesktopNavLinksInterface {
+  activeSection: string;
+  handleRefNavigation: (link: string) => void;
+}
+
+export interface MobileMenuInterface {
+  toggleMobileMenu: () => void;
+  isMobileMenuOpen: boolean;
+  lines: string[];
+}
+
+export interface MobileNavLinksInterface {
+  activeSection: string;
+  setIsMobileMenuOpen: Dispatch<SetStateAction<boolean>>;
+  handleRefNavigation: (link: string) => void;
 }
