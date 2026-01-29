276851627c4354039c1665516d07de5ba3e44d86
Deepesh Sunuwar <deepgeneral33@gmail.com>
2026-01-29 12:48:55 +0545

    chore: code clean up and portfolio initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
-import ImageUploader from "./ImageUploader/ImageUploader";
-
 const page = () => {
-  // const here = 100;
   return (
     <div>
       <div>
         kjasdlfkjsaf
         <div>
           <div>hello world</div>
-        </div>
-        <div className="w-[400px]">
-          <ImageUploader />
         </div>
       </div>
     </div>
