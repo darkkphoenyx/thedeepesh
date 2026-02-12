9f124a505e5d461896ccf9fedaecc997b909b6e1
Deepesh S. <140256020+darkkphoenyx@users.noreply.github.com>
2026-02-12 17:06:45 +0545

    Feat: Project section skeleton updated (#8)
    * fix: about section badge wrong import fix
    
    * feat: project skeleton updated

--- components/pages/homepage/projects-section/components/ProjectCardLoader.tsx
+++ components/pages/homepage/projects-section/components/ProjectCardLoader.tsx
@@ -2,30 +2,32 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const ProjectCardLoader = ({ cards }: { cards: number }) => {
   return (
-    <div className="bg-background hidden md:block">
-      <div className="mx-auto max-w-7xl grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 h-[700px] gap-8 px-4">
+    <div className="bg-background">
+      <div className="mx-auto max-w-7xl grid lg:grid-cols-2 grid-cols-1  gap-8">
         {Array(cards)
           .fill(null)
           .map((_, idx) => (
             <div
               key={idx}
-              className="h-full w-full transition-all rounded-3xl bg-secondary/20 animate-pulse p-8 flex flex-col gap-8"
+              className="h-full w-full transition-all rounded-2xl bg-secondary/20 animate-pulse max-md:p-4 p-8 flex flex-col gap-8"
             >
-              <Skeleton className="h-8 bg-secondary/30" />
-              <Skeleton className="h-14 bg-secondary/30" />
+              <Skeleton className="md:h-100 h-60 bg-secondary/30" />
+              <Skeleton className="h-10 bg-secondary/30" />
 
-              <div className="flex gap-2 w-full">
-                {Array(3)
-                  .fill(null)
-                  .map((_, idx) => (
-                    <Skeleton
-                      key={idx}
-                      className="h-9 w-full rounded-full bg-secondary/30"
-                    />
-                  ))}
+              <div className="grid md:grid-cols-4 gap-10">
+                <div className="flex gap-2 w-full col-span-3">
+                  {Array(3)
+                    .fill(null)
+                    .map((_, idx) => (
+                      <Skeleton
+                        key={idx}
+                        className="h-9 w-full rounded-md bg-secondary/30"
+                      />
+                    ))}
+                </div>
+
+                <Skeleton className="h-16 md:justify-self-end w-16 col-span-1 rounded-md bg-secondary/30" />
               </div>
-
-              <Skeleton className="h-10 rounded-full bg-secondary/30" />
             </div>
           ))}
       </div>
